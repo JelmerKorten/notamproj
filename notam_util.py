0db3d9cb826f41cfe89302ed5168a93d7aba817a
JelmerKorten <jelmer.korten@gmail.com>
2023-05-07 14:33:47 +0400

    add working cleanup

--- notam_util.py
+++ notam_util.py
@@ -3,6 +3,8 @@ import re
 import copy
 import pandas as pd
 import textwrap
+import arrow
+from pathlib import Path
 
 # imports for Create Circle point buffer
 from shapely.geometry import Point
@@ -682,10 +684,25 @@ def handle(filepath=None):
     back_traces(df,jdata)
 
 
-# %% TO IMPORT IN UILESS -- cleanup() projectdir -> deletes files older than 5 days
-def cleanup(today):
-    """To walk through files and output dir to delete files older than 5 days."""
-    
-    today
-    
-    pass
+# %% TO IMPORT IN UILESS -- cleanup() projectdir -> deletes files older than 5 days                
+def cleanup(DAYS):
+    """To walk through files and output dir to delete files older than DAYS days."""
+    # Get path, so that we can dynamically create the file paths
+    # for current OS
+    path = os.getcwd()
+    # Folders to check
+    folders = ['files', 'output']
+    # Set the time from when to remove
+    remove_time = arrow.now().shift(days=-DAYS)
+    for folder in folders:
+        # Join path of folders with cwd
+        folder_path = os.path.join(path, folder)
+        # For item in path that has _notams
+        for item in Path(folder_path).glob("*_notams*"):
+            # Double check if it's a file and not a directory
+            if not item.is_file():
+                continue
+            # Check if creation time of that file is more than DAYS days away
+            if arrow.get(item.stat().st_mtime) < remove_time:
+                # Remove the file
+                os.remove(item)
