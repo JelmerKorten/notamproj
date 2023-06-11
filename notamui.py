1dd960cee0b884de4c8328a715c40c2458774837
JelmerKorten <jelmer.korten@gmail.com>
2023-06-11 16:18:01 +0400

    add license

--- notamui.py
+++ notamui.py
@@ -1,3 +1,21 @@
+    # Notamplotter, plots notams on a streetmap.
+    # Copyright (C) 2023  Jelmer Korten (korty.codes)
+
+    # This program is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, either version 3 of the License, or
+    # (at your option) any later version.
+
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+
+    # You should have received a copy of the GNU General Public License
+    # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    # Contact: korty.codes@gmail.com
+
 # Notam gui
 # TODO: rewrite getnotams to fetch notams again if file already exists
 
