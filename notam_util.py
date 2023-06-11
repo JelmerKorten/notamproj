dd19cf1101ccb9208b1e6ba21ce1bb47d2e42e34
JelmerKorten <jelmer.korten@gmail.com>
2023-06-11 16:30:10 +0400

    add license

--- notam_util.py
+++ notam_util.py
@@ -1,18 +1,20 @@
-    # Notamplotter, plots notams on a streetmap.
+    # This file is part of NotamPlotter.
+
+    # NotamPlotter plots notams on a streetmap.
     # Copyright (C) 2023  Jelmer Korten (korty.codes)
 
-    # This program is free software: you can redistribute it and/or modify
+    # NotamPlotter is free software: you can redistribute it and/or modify
     # it under the terms of the GNU General Public License as published by
     # the Free Software Foundation, either version 3 of the License, or
     # (at your option) any later version.
 
-    # This program is distributed in the hope that it will be useful,
+    # NotamPlotter is distributed in the hope that it will be useful,
     # but WITHOUT ANY WARRANTY; without even the implied warranty of
     # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     # GNU General Public License for more details.
 
     # You should have received a copy of the GNU General Public License
-    # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    # along with NotamPlotter.  If not, see <https://www.gnu.org/licenses/>.
 
     # Contact: korty.codes@gmail.com
 
