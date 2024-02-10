18473df15b9e69fc725052bae5f46dc969a95e72
JelmerKorten <jelmer.korten@gmail.com>
2024-02-10 17:46:24 +0400

    removed code in test

--- tester.py
+++
@@ -1,3 +0,0 @@
-import notam_util as nu
-
-nu.handle(filepath_in='files/20230806_notams_omaa_omae_omad_omam.csv', filepath_out='output/20230806_notams_omaa_omae_omad_omam.html', airports_str="omaa omae omad omam")
